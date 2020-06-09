0b63620c2f36fcc21a43b50b52cae9a4ba9f1add
Shawn <shawnrstone1995@gmail.com>
2020-06-09 08:45:45 -0400

    cart updates and moved more compontents to redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -20,9 +20,6 @@ const Header = ({ currentUser, hidden }) => (
       <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className='option' to='/shop/hats'>
-        CONTACT
-      </Link>
       {currentUser ? (
         <div className='option' onClick={() => auth.signOut()}>
           SIGN OUT
