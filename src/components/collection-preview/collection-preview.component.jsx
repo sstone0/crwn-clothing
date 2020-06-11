55fcd1d65d31112bc430d4fee6c0621a0c838ee2
Shawn <shawnrstone1995@gmail.com>
2020-06-11 09:21:49 -0400

    shop data and directory to own reducer, created selectors and added product pages

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,9 +9,9 @@ const CollectionPreview = ({ title, items }) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className="preview">
             {items.filter((item, idx) => idx < 4)
-            .map((item) => (
-                <CollectionItem key={item.id} item={item}></CollectionItem>
-            ))}
+                .map((item) => (
+                    <CollectionItem key={item.id} item={item}></CollectionItem>
+                ))}
         </div>
     </div>
 )
